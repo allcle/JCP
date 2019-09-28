42934c9ed3c4b62cdb7a4b2c2c22916dc3398396
dfjung4254 <rmsghk4254@naver.com>
2019-09-28 20:46:39 +0900

    TimeHandler 클래스 구현, Handler 모듈화

---
+++ 2. Programming_workspace/HereHear/app/src/main/java/com/jcp/herehear/Class/TimeHandler.java
@@ -0,0 +1,52 @@
+package com.jcp.herehear.Class;
+
+import android.os.Handler;
+import android.os.Message;
+
+
+/*
+
+    Description : 진행시간 표현하는 Handler 클래스
+
+    Handler, AsyncTask 는 안드로이드 상에서 멀티쓰레드 프로그래밍을 통해
+    통신 처리, I/O로딩 처리, 타이머 등 실시간으로 동시에 처리되는 일들을
+    비동기적으로 UI 연동 처리까지 할 수 있는 방법입니다.
+    관련된 개념을 알아두신다면 꼭 안드로이드가 아니라도 프로그래밍적으로 많은 도움이 되실거에요
+    다음은 안드로이드의 Handler 클래스와 스레드에 대한 괜찮은 설명글 입니다 : https://recipes4dev.tistory.com/166
+
+    아래에 만들어놓은 클래스는
+    Handler + Callback 함수로 구현한 타이머 클래스입니다.
+    비동기처리에 대한 개념과 Callback 함수를 이해한다면 안드로이드 뿐만 아니라
+    서버쪽이든 클라이언트쪽이든 구현 지식이 많이 늘겁니다.
+
+    JAVA 에서는 interface 를 활용해서 콜백함수를 구현할 수 있습니다.
+    또한 안드로이드에 있는 많은 기능들이 콜백을 통해 구현되어있습니다 (onActivityResult 등)
+
+*/
+
+public class TimeHandler extends Handler {
+
+    /* UI callback 을 위한 인터페이스 */
+    public interface TimeHandleResponse{
+        void processUI();
+    }
+
+    /* 타임 핸들러를 요청한 해당 프레그먼트의 주소 값을 저장 */
+    public TimeHandleResponse delegate = null;
+
+    /* constructor */
+    public TimeHandler(TimeHandleResponse delegate){
+        this.delegate = delegate;
+    }
+
+    @Override
+    public void handleMessage(Message msg){
+
+        /* Call back */
+        delegate.processUI();
+
+    }
+
+
+
+}
--- 2. Programming_workspace/HereHear/app/src/main/java/com/jcp/herehear/Fragment/CryFragment.java
+++ 2. Programming_workspace/HereHear/app/src/main/java/com/jcp/herehear/Fragment/CryFragment.java
@@ -28,7 +28,9 @@ public class CryFragment extends Fragment {
     private TextView CrytxtTime;                       // 진행 시간 표시 뷰
 
     /* 생성자 */
-    public CryFragment() { }
+    public CryFragment() {
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -52,35 +54,33 @@ public class CryFragment extends Fragment {
     }
 
 
+    /*데이터: [시간, 소리크기] 리스트 Array*/
+    private class RecyclerAdapter extends RecyclerView.Adapter<ItemViewHolder> {
 
-/*데이터: [시간, 소리크기] 리스트 Array*/
-private class RecyclerAdapter extends RecyclerView.Adapter<ItemViewHolder>
-{
+        /* 임시 데이터 */
+        private ArrayList<CryData> listData = new ArrayList<>();
 
-    /* 임시 데이터 */
-    private ArrayList<CryData> listData = new ArrayList<>();
+        /* constructor - 임시 데이터 셋 생성 */
+        /* 추후 리스트에 나타낼 데이터의 용도에 맞게 따로 커스터마이징 해서 설정해주어야 함
+         * 현재 시안에는 cry에 시간, 소리크기가 나와있으므로 그것으로 잡음*/
+        public RecyclerAdapter() {
 
-    /* constructor - 임시 데이터 셋 생성 */
-    /* 추후 리스트에 나타낼 데이터의 용도에 맞게 따로 커스터마이징 해서 설정해주어야 함
-     * 현재 시안에는 cry에 시간, 소리크기가 나와있으므로 그것으로 잡음*/
-    public RecyclerAdapter() {
+            CryData tmp1 = new CryData("오전8:02", "80dB");
+            CryData tmp2 = new CryData("오전11:25", "140dB");
 
-        CryData tmp1 = new CryData("오전8:02", "80dB");
-        CryData tmp2 = new CryData("오전11:25", "140dB");
-
-        listData.add(tmp1);
-        listData.add(tmp2);
-    }
+            listData.add(tmp1);
+            listData.add(tmp2);
+        }
 
 
-    @NonNull
-    @Override
-    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        @NonNull
+        @Override
+        public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_cry_adapter, parent, false);
-        return new CryFragment.ItemViewHolder(view);
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_cry_adapter, parent, false);
+            return new CryFragment.ItemViewHolder(view);
 
-    }
+        }
 
         /*
             Adapter 리스트 내용이 notify 되었을 경우 onBindViewHolder 가 호출된다.
@@ -88,23 +88,23 @@ private class RecyclerAdapter extends RecyclerView.Adapter<ItemViewHolder>
             리스트뷰의 특정 목록의 데이터를 커스터마이징 한다.
         */
 
-    @Override
-    public void onBindViewHolder(@NonNull final ItemViewHolder holder, final int position) {
+        @Override
+        public void onBindViewHolder(@NonNull final ItemViewHolder holder, final int position) {
 
-        final CryData curData = listData.get(position);
+            final CryData curData = listData.get(position);
 
-        holder.whenCryText.setText(curData.getWhenCry());
-        holder.howCryText.setText(curData.getHowCry());
+            holder.whenCryText.setText(curData.getWhenCry());
+            holder.howCryText.setText(curData.getHowCry());
+
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return listData.size();
+        }
 
     }
-
-
-    @Override
-    public int getItemCount() {
-        return listData.size();
-    }
-
-}
 
     private class ItemViewHolder extends RecyclerView.ViewHolder {
 
